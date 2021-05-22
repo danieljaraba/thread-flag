815379373ca4bdac16e5f553173c1d41c45bbd52
juan jose jaraba gaviria <daesjaga2003@gmail.com>
2021-05-21 20:31:37 -0500

    UML Diagram

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -6,14 +6,20 @@ import threads.FlagThread;
 public class Main {
 
     private final static String ESC = "\u001b[";
+    private static Flag flag1;
+    private static Flag flag2;
+    private static Flag flag3;
+    private static FlagThread thread1;
+    private static FlagThread thread2;
+    private static FlagThread thread3;
 
     public static void main(String[] args) throws InterruptedException {
-        Flag flag1 = new Flag(43, " ");
-        Flag flag2 = new Flag(44, " ");
-        Flag flag3 = new Flag(41, " ");
-        FlagThread thread1 = new FlagThread(flag1, 5, 0, 3);
-        FlagThread thread2 = new FlagThread(flag2, 2, 5, 10);
-        FlagThread thread3 = new FlagThread(flag3, 2, 7, 12);
+        flag1 = new Flag(43, " ");
+        flag2 = new Flag(44, " ");
+        flag3 = new Flag(41, " ");
+        thread1 = new FlagThread(flag1, 4, 1, 3);
+        thread2 = new FlagThread(flag2, 2, 5, 10);
+        thread3 = new FlagThread(flag3, 2, 7, 12);
 
         System.out.print(ESC+"2J");
         thread1.start();
